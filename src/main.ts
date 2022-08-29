fb792146c931d2f3bc4a0c39740dc63e34c102cf
Nikita Lepeykin <lepeykin@arview.pro>
2022-08-29 16:34:14 +0700

    initial

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 5000
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT).then(() =>
+    console.log(`Server started at port ${PORT}`)
+  );
+}
+bootstrap();
